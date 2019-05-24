d917dfd91bdcb3f74c651b7875273f6f17a56777
Ksenia <ksenia.malchikova@gmail.com>
2019-05-24 23:06:34 +0300

    Update 6-1

--- 6-1.cpp
+++ 6-1.cpp
@@ -1,3 +1,5 @@
+//Класс точка в N-мерном пространстве. N задается как глобальная константа.
+
 #include <iostream>
 #include <cstring>
 
@@ -6,24 +8,24 @@ using namespace std;
 const int n {4};
 
 class Point {
-    int* coord;//Реально? int??????? хм...
+private:
+    double coord[n];
     
 public:
     string name;
     
-    Point(int* c)//очень неочевидный и странный способ инициализации
+    Point(const Point &other)
     {
-        coord = new (nothrow) int[n];
-        for (int i = 0; i < n; ++i)
-            coord[i] = c[i];
+        for (int i = 0; i < n; ++i) coord[i] = other.coord[i];
+        
     }
     
-    Point()
+    Point() : coord{0}{};
+    
+    Point(double k)
     {
-        coord = new (nothrow) int[n];
-        for (int i = 0; i < n; ++i)
-            coord[i] = 0;
-    }
+        for (int i = 0; i < n; ++i) coord[i] = k;
+    };
     
     void Print()
     {
@@ -33,12 +35,12 @@ public:
         cout << coord[n-1];
         cout << ")";
         cout << endl;
-    }
+    };
     
     ~Point()
     {
         cout << "Destructor is working.";
-    }
+    };
     
     const Point& operator = (const Point& point1)
     {
@@ -63,11 +65,11 @@ public:
         return (diff);
     };
     
-    const Point operator * (const Point point1)// Умножение точек??? Я бы понял умножение на число!!!!
+    const Point operator * (const double num)
     {
         Point mlt;
         for (int i = 0; i < n; ++i)
-            mlt.coord[i] = point1.coord[i] * coord[i];
+            mlt.coord[i] = coord[i] * num;
         return (mlt);
     };
     
@@ -93,10 +95,10 @@ public:
         for (int i = 0; i < n; ++i)
         {
             if (point1.coord[i] == coord[i])
-                tmp = true;//зачем?????
+                tmp = true;
             else
             {
-            return false;
+                return false;
             }
         }
         return true;
@@ -113,24 +115,25 @@ int main() {
         exit(1);
     }
     
-    int a[n] = {1,2,3,4};
+    //int a[n] = {1,2,3,4};
     
-    Point point1(a);
+    Point point1(3);
     point1.name = "a";
     cout << point1.name <<" = ";
     point1.Print();
     
-    int b[n] = {6,-1,4,3};
+    //int b[n] = {6,-1,4,3};
     
-    Point point2(b);
-    point2.name = "b";
-    cout << point2.name <<" = ";
-    point2.Print();
+    Point *point2;
+    point2 = &point1;
+    point2->name = "b";
+    cout << point2->name <<" = "<<*point2;
+    //point2->Print();
     
     Point point3[2]
     {
-        Point(a),
-        Point(b),
+        Point(5),
+        Point(6),
     };
     point3[0].name = "a1";
     cout << point3[0].name <<" = ";
@@ -139,34 +142,30 @@ int main() {
     cout << point3[1].name <<" = ";
     point3[1].Print();
     
-    Point* point4 = new Point(a);
+    Point* point4 = new Point(3);
     point4->name = "a2";
     cout << point4->name << " = ";
     point4->Print();
     
-    Point* point5 = &point2;
-    point5->name = "b2";
-    cout << point5->name <<" = ";
-    point5->Print();
     
     Point point6;
-    point6 = point1 + point2;
+    point6 = point1 + *point2;
     cout << endl << "The sum of the coordinates of point1 and point2: ";
     point6.Print();
     
     Point point7;
-    point7 = point1 - point2;
+    point7 = point1 - *point2;
     cout << endl << "The division of the coordinates of point1 and point2: ";
     point7.Print();
-  
+    
     Point point8;
-    point8 = *point5;
-    cout << "point8 = point5 = (";
+    point8 = *point3;
+    cout << "point8 = point3 = (";
     cout << point8 << ")" << endl;
     
     Point point9;
-    point9 = point1 * point2;
-    cout << endl << "The multiplication of the coordinates of point1 and point2: ";
+    point9 = point1 * 5;
+    cout << endl << "The multiplication of the coordinates of point1 and 5: ";
     point9.Print();
     
     Point point10;
@@ -174,7 +173,7 @@ int main() {
     cout << "(" << point10 << ")" << endl;
     
     if (point8 == point7) cout << endl << "Point7 is equal point8" << endl;
-        else cout << endl << "Point7 is not equal point8" << endl;
+    else cout << endl << "Point7 is not equal point8" << endl;
     
     delete point4;
     delete[] coord;
@@ -182,3 +181,4 @@ int main() {
     
     return 0;
 }
+
