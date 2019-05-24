1c25b5d8cafe77916eab904a3eec9d56b0aecccb
Ksenia <ksenia.malchikova@gmail.com>
2019-05-24 23:09:45 +0300

    Update 2-1.cpp

--- 2-1.cpp
+++ 2-1.cpp
@@ -3,41 +3,51 @@
 #include <iostream>
 using namespace std;
 
-int** newArray(int rows, int cols) {
+int** newArray(int rows, int cols)
+{
     int **arr = new(nothrow) int* [rows];
-    if (!arr) {
+    if (!arr)
+    {
         cout << "Could not allocate memory" << '\n';
         exit(EXIT_FAILURE);
     }
-        for (int i=0; i<rows; i++) {
-          arr[i] = new int [cols];
-            if (!arr[i]) {
-                cout << "Could not allocate memory" << '\n';
-                exit(EXIT_FAILURE);
-            }
+    for (int i=0; i<rows; i++)
+    {
+        *(arr + i) = new(nothrow) int [cols];
+        if (!*(arr + i))
+        {
+            cout << "Could not allocate memory" << '\n';
+            exit(EXIT_FAILURE);
         }
+    }
     return arr;
 }
 
-void deleteA(int** arr, int rows) {
-    for (int i=0; i<rows; i++){
-        delete[] arr[i];
+void deleteA(int**& arr, int rows)
+{
+    for (int i=0; i<rows; i++)
+    {
+        delete[] *(arr + i);
     }
-    delete[] arr; 
+    delete[] arr;
     arr = nullptr;
 }
 
 void inputA(int **arr, int rows, int cols)
 {
-    for (int** i = arr; i < arr + rows ; i++) {
-        for (int* j = *i; j < *i + cols; j++){
+    for (int** i = arr; i < arr + rows ; i++)
+    {
+        for (int* j = *i; j < *i + cols; j++)
+        {
             cin >> *j;
         }
     }
 }
 
-void printA(int** arr, int rows, int cols) {
-    for (int** i = arr; i < arr + rows; i++) {
+void printA(int** arr, int rows, int cols)
+{
+    for (int** i = arr; i < arr + rows; i++)
+    {
         cout << '\n';
         for (int* j = *i; j < *i + cols; j++)
             cout << *j << " ";
@@ -45,30 +55,31 @@ void printA(int** arr, int rows, int cols) {
     cout << '\n';
 }
 
-void moveRows(int** arr, int rows, int cols){
+void moveRows(int** arr, int rows, int cols)
+{
     int max = **arr, min = **arr;
     long int max_row = 0, min_row = 0;
-    int A;
-    for (int** i = arr; i< arr + rows; i++){
-        for (int* j = *i; j < *i + cols; j++){
-            A = *j;
-            if (A >= max) {
-                max = A;
+    for (int** i = arr; i< arr + rows; i++)
+    {
+        for (int* j = *i; j < *i + cols; j++)
+        {
+            if (*j >= max)
+            {
+                max = *j;
                 max_row = i - arr;
             }
-            else if (A <= min){
-                min = A;
+            else if (*j <= min)
+            {
+                min = *j;
                 min_row = i - arr;
             }
         }
     }
-    
-    for (int j = 0; j < cols ; j++){
-        swap(*(*(arr+max_row)+j), *(*(arr+min_row)+j));//совсем плохо... Это то, что мы делали в 1 семестре. А здесь надо поменять СТРОКИ. Указатели не используешь!!!!
-    }
+    swap(*(arr+min_row),*(arr+max_row));
 }
 
-int main() {
+int main()
+{
     
     int rows, cols;
     cout << "Enter the number of rows and columns" << '\n';
@@ -80,4 +91,3 @@ int main() {
     deleteA(arr, rows);
     return 0;
 }
-
